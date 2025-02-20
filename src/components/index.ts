1823517470225c1d922dab9d01adfa78de59009b
wen2720 <wenhao2720@gmail.com>
2025-02-20 16:52:03 +0100

    test CI work flow

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-export { default as SidePlane } from './app/left_col'
-export { default as MainCol } from './app/right_col';
+export { default as LeftColumn } from './app/left_col'
+export { default as RightColumn } from './app/right_col';
 export { ColorBox, type IconText} from './color_box'
 export { default as IconLabel} from './icon_label'
 export { EventBox, type WorkExp} from "./event_box"
