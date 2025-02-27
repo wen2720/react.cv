f583c7ac0a817f3b4699a09031c6bab81c5f2280
wen2720 <wenhao2720@gmail.com>
2025-02-27 20:16:51 +0100

    change tailwind color to inline style

--- src/components/link_text.tsx
+++ src/components/link_text.tsx
@@ -11,7 +11,7 @@ const LinkText:React.FC<TextLink> = (
         link === undefined?	
         id
         :
-        <a className={"text-blue-600"} href={link}>{id}</a>
+        <a style={{ color: '#2563eb' }} href={link}>{id}</a>
       }
   </>
 )
